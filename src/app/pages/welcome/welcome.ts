c5a9545e32d4026bed09c69e71f9cfbe5c7c2dfd
Max Alva <alva85@gmail.com>
2026-02-26 17:06:25 -0500

    feat: agregar componente loader y estado de carga en welcome

--- src/app/pages/welcome/welcome.ts
+++ src/app/pages/welcome/welcome.ts
@@ -2,10 +2,11 @@ import { Component, computed, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { VisitorsService } from '../../services/visitors.service';
 import { ButtonDirective } from '../../directives/button';
+import { Loader } from '../../components/loader/loader';
 
 @Component({
   selector: 'app-welcome',
-  imports: [ButtonDirective],
+  imports: [ButtonDirective, Loader],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss',
 })
@@ -13,6 +14,7 @@ export class Welcome {
   private readonly router = inject(Router);
   private readonly visitorsService = inject(VisitorsService);
 
+  protected readonly loading = this.visitorsService.loading;
   protected readonly visitor = this.visitorsService.visitor;
   protected readonly welcome = computed(() => this.visitor()?.welcome ?? '');
   protected readonly version = computed(() => this.visitor()?.version ?? '');
--- src/app/services/visitors.service.ts
+++ src/app/services/visitors.service.ts
@@ -7,12 +7,19 @@ import { VisitorData, VisitorResponse } from '../interfaces/visitor.interface';
 export class VisitorsService {
   private readonly http = inject(HttpClient);
   private readonly _visitor = signal<VisitorData | null>(null);
+  private readonly _loading = signal(false);
 
   readonly visitor = this._visitor.asReadonly();
+  readonly loading = this._loading.asReadonly();
 
   getVisitor() {
+    this._loading.set(true);
     this.http.post<VisitorResponse>(`${environment.apiUrl}/visitors`, {}).subscribe({
-      next: (res) => this._visitor.set(res.data),
+      next: (res) => {
+        this._visitor.set(res.data);
+        this._loading.set(false);
+      },
+      error: () => this._loading.set(false),
     });
   }
 }
